f26a4e4004550efafff7aeb83a1de713d4cb4ea8
Ryan Chang <anakinchung930@gmail.com>
2022-09-26 13:44:08 +0800

    update

--- registration/static/js/761.e335133c.js
+++ registration/static/js/761.e335133c.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkregistration"]=self["webpackChunkregistration"]||[]).push([[761],{2761:function(e,t,n){n.r(t),n.d(t,{default:function(){return d}});var r=n(3396);function a(e,t,n,a,u,i){const s=(0,r.up)("router-view");return(0,r.wg)(),(0,r.iD)("div",null,[(0,r.Wm)(s)])}var u=n(4870),i=n(3927),s=n(678),c=(0,r.aZ)({setup(){const e=new i.L,t=(0,s.yj)(),n=(0,u.iH)({});function r(){e.Url(`games/${t.params.game_id}`).GetAll(n)}return r(),{gameData:n}}}),o=n(89);const l=(0,o.Z)(c,[["render",a],["__scopeId","data-v-77e6b1e2"]]);var d=l}}]);
-//# sourceMappingURL=761.62646892.js.map
+//# sourceMappingURL=761.e335133c.js.map
